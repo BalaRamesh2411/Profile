742f36e482b384ce02f3442fb528e2347c5500f8
BalaRamesh2411 <balaramesh2411@gmail.com>
2024-12-30 19:21:27 +0530

    add my achievement

--- src/components/resume/Achievement.js
+++ src/components/resume/Achievement.js
@@ -11,31 +11,38 @@ const Achievement = () => {
     >
       <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2007 - 2010</p>
-          <h2 className="text-3xl md:text-4xl font-bold">Company Experience</h2>
+          {/* <p className="text-sm text-designColor tracking-[4px]">2007 - 2010</p> */}
+          <h2 className="text-3xl md:text-4xl font-bold">Achievement Experience</h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
+            title="Sustainable Diabetic Retinopathy Diagnosis System Using IOT"
+            subTitle="Published by:-INTERNATIONALRESEARCH JOURNAL OFMULTIDISCIPLINARYTECHNOVATION(IRJMT) DOI:10.34256/irjmtcon10"
             result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
+            des="The system utilizes sustainable computing to provide remote access
+              for quick implementation, measuring glucose levels through Dexcom
+              G4 Platinum sensors on diabetic patients. IoT platforms offer a
+              sustainable solution for Diabetic Retinopathy, aiming to prevent vision
+              loss. The platform securely collects and analyzes vast data from IoT
+              devices, predicting valuable clinical insights. The proposed technique
+              achieves 99.58% accuracy, 72.51% sensitivity, and 99.83% specificity,
+              outperforming other methods in experimental settings.!"
           />
           <ResumeCard
+            title="Evaluation of the Control System."
+            subTitle="Workshop."
+            result="Success"
+            des="Evaluation of the Control System refers to the process of assessing the performance, effectiveness, and efficiency of a control system to ensure it meets its intended objectives. This involves analyzing the system’s design, functionality, response time, accuracy, stability, and adaptability. The evaluation helps identify any issues, improve control mechanisms, and ensure optimal performance and reliability in achieving desired outcomes.!"
+          />
+          {/* <ResumeCard
             title="Lorem ipsum dolor sit amet."
             subTitle="Lorem ipsum dolor sit amet alternative."
             result="Success"
             des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
-          />
-          <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
-            result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
-          />
+          /> */}
         </div>
       </div>
-      <div>
+      {/* <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
           <p className="text-sm text-designColor tracking-[4px]">2007 - 2010</p>
           <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
@@ -60,7 +67,7 @@ const Achievement = () => {
             des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
           />
         </div>
-      </div>
+      </div> */}
     </motion.div>
   );
 };
