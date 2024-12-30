057608fd2e1d75d138779d48cfad236dc279a68c
BalaRamesh2411 <balaramesh2411@gmail.com>
2024-12-30 20:11:13 +0530

    add images

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,14 @@ import logo1 from "./images/logo1.png";
 import bannerImg from "./images/bannerImg.png";
 import projectOne from "./images/projects/projectOne.jpg";
 import projectTwo from "./images/projects/projectTwo.jpg";
-import projectThree from "./images/projects/projectThree.jpeg";
+import projectThree from "./images/projects/projectThree.jpg";
+import projectFour from "./images/projects/projectFour.jpg";
+import projectFive from "./images/projects/projectFive.jpg";
+import projectSix from "./images/projects/projectSix.jpg";
+import projectSeven from "./images/projects/projectSeven.jpg";
+import projectNine from "./images/projects/projectNine.jpg";
+import projectEight from "./images/projects/projectEight.jpg";
+import projectTen from "./images/projects/projectTen.jpg";
 import testimonialOne from "./images/testmonial/testimonialOne.png";
 import testimonialTwo from "./images/testmonial/testimonialTwo.png";
 import quote from "./images/testmonial/quote.png";
@@ -15,6 +22,13 @@ export {
   projectOne,
   projectTwo,
   projectThree,
+  projectFour,
+  projectFive,
+  projectSix, 
+  projectSeven,
+  projectEight, 
+  projectNine,
+  projectTen ,
   testimonialOne,
   testimonialTwo,
   quote,
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Title from "../layouts/Title";
-import { projectOne, projectTwo, projectThree } from "../../assets/index";
+import { projectOne, projectTwo, projectThree ,projectFour ,projectFive, projectSix, projectSeven,projectEight, projectNine,projectTen  } from "../../assets/index";
 import ProjectsCard from "./ProjectsCard";
 // import { BsGithub } from "react-icons/bs";
 // import { FaGlobe } from "react-icons/fa";
@@ -52,7 +52,7 @@ const Projects = () => {
                 achieves 99.58% accuracy, 72.51% sensitivity, and 99.83% specificity,
                 outperforming other methods in experimental settings.
                 "
-          src={projectTwo}
+          src={projectThree}
         />
         <ProjectsCard
           title="RESUME BUILDER WITH JAVASCRIPT AND FIREBASE"
@@ -63,7 +63,7 @@ const Projects = () => {
             resumes effortlessly. JavaScript for frontend interactivity, Bootstrap for
             responsive design implementation and Firebase for real-time database
             management."
-          src={projectTwo}
+          src={projectFour}
         />
         <ProjectsCard
           title="Chatting App"
@@ -71,13 +71,13 @@ const Projects = () => {
               A Chatting App is a communication platform that enables real-time text, voice, or video messaging between users. 
               It features user authentication, message encryption, and chatroom creation, ensuring secure and seamless interaction.
               Common functionalities include file sharing, group chats, notifications, and customizable user profiles."
-          src={projectTwo}
+          src={projectFive}
         />
         <ProjectsCard
           title="BMI_APP"
           des="A BMI Calculator App is a simple tool that helps users calculate their Body Mass Index (BMI) based on their height and weight. It provides quick insights into their health category, such as underweight, normal weight, overweight, or obese.
            The app ensures a user-friendly interface with accurate results and personalized recommendations."
-          src={projectThree}
+          src={projectSix}
         />
         <ProjectsCard
           title="Access-management"
@@ -90,28 +90,28 @@ const Projects = () => {
           title="Digital-Clock"
           des=" A Digital Clock is a real-time timekeeping application that displays the current time in a digital format (HH:MM:SS). It updates dynamically and can include features like 12-hour/24-hour format toggling, date display, and customizable themes. 
           Digital clocks are commonly used in web, mobile, or desktop applications for convenience and accessibility."
-          src={projectTwo}
+          src={projectSeven}
         />
         <ProjectsCard
           title="EmiCalculator"
           des=" An EMI Calculator is a financial tool that helps users calculate the Equated Monthly Installments (EMI) for loans. By inputting the loan amount, interest rate, and tenure, users can instantly determine their monthly repayment amount. The app provides a clear breakdown of principal and interest, aiding in better financial planning and decision-making."
-          src={projectTwo}
+          src={projectEight}
         />
         <ProjectsCard
           title="currency-converter"
           des=" A Currency Converter is an application that allows users to convert one currency to another based on current exchange rates. It provides real-time updates and supports multiple currencies, enabling users to make accurate financial decisions for travel, business, or trading. The app often includes features like historical data, favorite currency pairs, and offline mode for convenience."
-          src={projectTwo}
+          src={projectNine}
         />
         <ProjectsCard
           title="WeatherApp"
           des=" A Weather App is an application that provides users with real-time weather updates for their selected locations. It displays information such as temperature, humidity, wind speed, and weather conditions (e.g., sunny, rainy). Advanced features may include forecasts, radar maps, and alerts for severe weather, offering convenience and preparedness for users."
-          src={projectTwo}
+          src={projectTen}
         />
 
         <ProjectsCard
           title="CURD App"
           des=" A Curd App is a mobile or web application designed to help users manage their curd (yogurt) production process, track fermentation times, temperature settings, and ensure optimal conditions for making curd. It may provide features like reminders, recipe suggestions, and tips to improve the quality and consistency of homemade curd."
-          src={projectTwo}
+          src={projectOne}
         />
       </div>
     </section>
