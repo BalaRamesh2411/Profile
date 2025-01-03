92155d6ad8cc99a734684906e59d18ee2bda8158
BalaRamesh2411 <balaramesh2411@gmail.com>
2025-01-03 13:24:37 +0530

    AchievemenJOURNALt change

--- src/components/resume/Achievement.js
+++ src/components/resume/Achievement.js
@@ -16,23 +16,16 @@ const Achievement = () => {
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Sustainable Diabetic Retinopathy Diagnosis System Using IOT"
-            subTitle="Published by:-INTERNATIONALRESEARCH JOURNAL OFMULTIDISCIPLINARYTECHNOVATION(IRJMT) DOI:10.34256/irjmtcon10"
+            title="JOURNAL Puplished"
+            subTitle="Sustainable Diabetic Retinopathy Diagnosis System Using IOT"
             result="Success"
-            des="The system utilizes sustainable computing to provide remote access
-              for quick implementation, measuring glucose levels through Dexcom
-              G4 Platinum sensors on diabetic patients. IoT platforms offer a
-              sustainable solution for Diabetic Retinopathy, aiming to prevent vision
-              loss. The platform securely collects and analyzes vast data from IoT
-              devices, predicting valuable clinical insights. The proposed technique
-              achieves 99.58% accuracy, 72.51% sensitivity, and 99.83% specificity,
-              outperforming other methods in experimental settings.!"
+            des="Published by:-INTERNATIONALRESEARCH JOURNAL OFMULTIDISCIPLINARYTECHNOVATION(IRJMT) DOI:10.34256/irjmtcon10"
           />
           <ResumeCard
             title="Evaluation of the Control System."
             subTitle="Workshop."
             result="Success"
-            des="Evaluation of the Control System refers to the process of assessing the performance, effectiveness, and efficiency of a control system to ensure it meets its intended objectives. This involves analyzing the system’s design, functionality, response time, accuracy, stability, and adaptability. The evaluation helps identify any issues, improve control mechanisms, and ensure optimal performance and reliability in achieving desired outcomes.!"
+            des="Evaluation of the Control System refers to the process of assessing the performance, effectiveness, and efficiency of a control system to ensure it meets its intended objectives. This involves analyzing the system’s design, functionality, response time, accuracy, stability, and adaptability. !"
           />
           {/* <ResumeCard
             title="Lorem ipsum dolor sit amet."
